06da8dfd3c0da2868217ff20f2cf595b8249cf28
Norawit Urailertprasert <wit543@gmail.com>
2016-03-04 23:28:57 +0700

    fix error

--- laba1-2/alu.h
+++ laba1-2/alu.h
@@ -6,50 +6,25 @@
 
 SC_MODULE(alu) {
 	sc_in<sc_uint<16> > ain, bin;
-	sc_signal<bool> ci, as;
+	sc_signal<bool> ciS, as, coS;
+	sc_signal<bool> ciA, coA;
 	sc_in<sc_uint<4>> control;
 	sc_out<sc_uint<16> > sum;
 	sc_out<bool> zflag, oflag, lflag;
-	sc_signal<bool> co;
+	sc_signal<bool> zflagA, oflagA, lflagA;
+	sc_signal<bool> zflagB, oflagB, lflagB;
 	sc_signal<sc_uint<16>> sum_add, sum_sub;
 	sc_uint<16> sum_xor, sum_or, sum_nota, sum_stl, sum_and;
-	 
+	add_sub DUT,SUBB;
 
-	void ADD() {
-		add_sub DUT("add_sub");
-		DUT.ain(ain);
-		DUT.bin(bin);
-		DUT.ci(ci);
-		as.write(false);
-		DUT.as(as);
-		DUT.sum(sum_add);
-		DUT.co(co);
-		DUT.zflag(zflag);
-		DUT.oflag(oflag);
-		DUT.lflag(lflag);
-
-	}
-
-	void SUB() {
-		add_sub adder("add_sub");
-		adder.ain(ain);
-		adder.bin(bin);
-		adder.ci(ci);
-		as.write(true);
-		adder.as(as);
-		adder.sum(sum_sub);
-		adder.co(co);
-		adder.zflag(zflag);
-		adder.oflag(oflag);
-		adder.lflag(lflag);
-	}
 	sc_uint<16> XOR(sc_uint<16> ain, sc_uint<16> bin) {
 		return ain ^ bin;
 	}
 
 	void AND(/* arguments */) {
 		/* code */
-		sum_and = ain.read() & bin.read();
+		for (int i = 0; i < 16;i++)
+			sum_and = ain.read() & bin.read();
 	}
 	void OR(/* arguments */) {
 		/* code */
@@ -59,30 +34,36 @@ SC_MODULE(alu) {
 		/* code */
 		sum_nota = ~ain.read();
 	}
-	void STL(/* arguments */) {
-		/* code */
-		add_sub adder("add_sub");
-		adder.ain(ain);
-		adder.bin(bin);
-		adder.ci(ci);
-		as.write(true);
-		adder.as(as);
-		adder.sum(sum_add);
-		adder.co(co);
-		adder.zflag(zflag);
-		adder.oflag(oflag);
-		adder.lflag(lflag);
-		sum_stl[0] = oflag;
-
-	}
 
 	void run(/* arguments */) {
-		ADD();
+		
 	}
 
-	SC_CTOR(alu) {
+	SC_CTOR(alu): DUT("add") ,SUBB("sub"){
+
+		DUT.ain(ain);
+		DUT.bin(bin);
+		DUT.ci(ciA);
+		as.write(false);
+		DUT.as(as);
+		DUT.sum(sum_add);
+		DUT.co(coA);
+		DUT.zflag(zflagA);
+		DUT.lflag(lflagA);
+		DUT.oflag(oflagA);
+
+		SUBB.ain(ain);
+		SUBB.bin(bin);
+		SUBB.ci(ciS);
+		as.write(true);
+		SUBB.as(as);
+		SUBB.sum(sum_sub);
+		SUBB.co(coS);
+		SUBB.zflag(zflagB);
+		SUBB.oflag(oflagB);
+		SUBB.lflag(lflagB);
 		SC_METHOD(run);
-		sensitive << ain << bin << ci << as;
+		sensitive << ain << bin << ciA << as << control;
 	}
 
 
